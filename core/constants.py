4cc9552b5c09a7f2778d764bd7da33f1b0a0d20d
crazia <crazia@gmail.com>
2018-06-03 23:09:45 +0900

    option optimizing

--- core/constants.py
+++ core/constants.py
@@ -1,7 +1,6 @@
-out_dir='/Users/crazia/work/data-projcts/data/model'
+out_dir='../../data/model'
 override_loaded_hparams=True
 hparams_path=None
 ckpt=""
-src_vocab_file='/Users/crazia/work/data-projcts/data/vocab.req'
-tgt_vocab_file='/Users/crazia/work/data-projcts/data/vocab.rep'
+
 
