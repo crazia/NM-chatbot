2c987e67b64f450c9cad4ac970674e2344752443
crazia <crazia@getaim.co>
2018-06-07 14:24:53 +0900

    admin site added

--- chat/admin.py
+++ chat/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Category, Chat
 # Register your models here.
 
+
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ('category', 'question', 'answer', 'verify')
+    list_filter = ['verify', 'category']
+
+
 admin.site.register(Category)
-admin.site.register(Chat)
+admin.site.register(Chat, ChatAdmin)
