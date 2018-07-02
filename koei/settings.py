8f27f879455de405b32c2098d10225f10377dfba
rocketpunch <rocketpunch.dev@gmail.com>
2018-07-02 17:22:32 +0900

    Add logs Directory / Allow hosts / Modify .gitignore

--- koei/settings.py
+++ koei/settings.py
@@ -15,8 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -26,15 +24,16 @@ SECRET_KEY = 'n#=r8khqrfp^qqy%-u5zei7ew&s8lmgba6)zja6xq@6vee^qye'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['chat.crazia.org',
-                 'localhost',
-                 '127.0.0.1',
-                 '13.124.79.14',
-                 'srv.crazia.org', ]
-
+ALLOWED_HOSTS = [
+    'chat.crazia.org',
+    'chatbot.rocketpunch.kr',
+    'localhost',
+    '127.0.0.1',
+    '13.124.79.14',
+    'srv.crazia.org',
+]
 
 # Application definition
-
 INSTALLED_APPS = [
     'bootstrap_admin',
     'django.contrib.admin',
