acabed76539f02144a81a487416ccfe1812ee9f5
rocketpunch <rocketpunch.dev@gmail.com>
2018-08-13 12:21:19 +0900

    fixed: sent, replies position / set mobile size

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.utils.safestring import mark_safe
-import json
 
-# Create your views here.
 
 def index(request):
     return render(request, 'chat/index.html', {})
